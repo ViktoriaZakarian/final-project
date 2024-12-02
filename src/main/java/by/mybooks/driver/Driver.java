cb5b75a939907afebab3d712345395a800104e06
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-02 23:00:25 +0300

    add Driver

---
+++ src/main/java/by/mybooks/driver/Driver.java
@@ -0,0 +1,30 @@
+package by.mybooks.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    public Driver() {
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        return driver;
+    }
+
+    public static void quit() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}

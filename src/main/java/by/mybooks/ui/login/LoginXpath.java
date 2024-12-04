802b28ed2c4c19e41c56aaed350665c244b6f64f
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-05 00:49:00 +0300

    refactor LoginXpath

--- src/main/java/by/mybooks/ui/login/LoginXpath.java
+++ src/main/java/by/mybooks/ui/login/LoginXpath.java
@@ -3,7 +3,9 @@ package by.mybooks.ui.login;
 public class LoginXpath {
 
     public static final String BUTTON_ENTRANCE_HOMEPAGE_XPATH = "//span[@class='ok-auth__info']";
-    public static final String INPUT_EMAIL_XPATH = "//div[text()='Ваш email']";
-    public static final String INPUT_PASSWORD_XPATH = "//div[text()='Пароль']";
+    public static final String INPUT_EMAIL_XPATH = "//div[@id='enter']//*//input[@name='log_email']";
+    public static final String INPUT_PASSWORD_XPATH = "//div[@id='enter']//*//input[@name='log_password']";
+    public static final String EMAIL_MESSAGE_XPATH = "//div[@id='enter']//*//div[text()='Ваш email']";
+    public static final String PASSWORD_MESSAGE_XPATH = "//div[@id='enter']//*//div[text()='Пароль']";
     public static final String BUTTON_ENTRANCE_XPATH = "//button[contains(text(),'Вход')]";
 }

9eff6334d0e0ed97d8890e343be4f18a5260d6cf
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-11 22:08:18 +0300

    add HomePageMessage

---
+++ src/main/java/by/mybooks/ui/homepage/HomePageMessage.java
@@ -0,0 +1,8 @@
+package by.mybooks.ui.homepage;
+
+public class HomePageMessage {
+
+    public static final String ADDRESS_OF_THE_SHOP_TEXT = "Г. МИНСК, ОКТЯБРЬСКАЯ ПЛ. 2, ТЦ КУПАЛОВСКИЙ, ";
+    public static final String THE_TITLE_OF_THE_BOOK_IN_THE_BASKET = "Джоан Роулинг. Гарри Поттер и философский камень " +
+            "(оригинал Росмэн) | Роулинг Дж.К.";
+}

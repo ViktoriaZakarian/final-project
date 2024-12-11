2015d72a1ae0d225b9a65e413b13d0f22a02ca86
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-11 22:07:54 +0300

    refactor HomePageXpath

--- src/main/java/by/mybooks/ui/homepage/HomePageXpath.java
+++ src/main/java/by/mybooks/ui/homepage/HomePageXpath.java
@@ -2,9 +2,12 @@ package by.mybooks.ui.homepage;
 
 public class HomePageXpath {
 
+    public static final String BUTTON_ACCEPT_ALL_COOKIES_XPATH = "//button[@data-button-ck-action='accept-all']";
     public static final String ADVANCED_SEARCH_LINK_XPATH = "//span[text()='Расширенный поиск']";
     public static final String PAYMENT_AND_DELIVERY_LINK_XPATH = "//span[text()='Оплата и доставка']";
     public static final String ADDRESS_OF_THE_SHOP_LINK_XPATH = "//span[text()='Адрес магазина']";
+    public static final String ADDRESS_OF_THE_SHOO_TEXT_XPATH = "//div[@style=' padding: 20px; font-family: -webkit-body; ']" +
+            "//span[contains(text(), 'Г. МИНСК, ОКТЯБРЬСКАЯ ПЛ. 2, ТЦ КУПАЛОВСКИЙ')]";
     public static final String SEARCH_STRING_XPATH = "//input[@placeholder='поиск']";
     public static final String BUTTON_SEARCH_XPATH = "//button[@type='submit']";
     public static final String IN_STOCK_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='В наличии']";
@@ -14,4 +17,9 @@ public class HomePageXpath {
     public static final String FICTION_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Художественная литература']";
     public static final String ESOTERICISM_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Эзотерика']";
     public static final String PSYCHOLOGY_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Психология']";
+    public static final String BOOK_HARRY_POTTER_XPATH = "//div[@data-cart-id='798787']//div[@class='ok-product__main ']";
+    public static final String BUTTON_ADD_BOOK_INTO_BASKET_XPATH = "//meta[@data-id='1355176']//..//button" +
+            "[@class='ok-product__add-shcart ok-btn -btn-shopping-cart ok-product__btn v-kozinu']";
+    public static final String BUTTON_BASKET_XPATH = "//div[@class='ok-shcart-box']";
+    public static final String BOOK_IN_THE_BASKET_XPATH = "//a[@href='/bkart/id-1034559431/']";
 }

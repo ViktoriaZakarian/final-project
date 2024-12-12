0a90421cbddd0dae38b607bdf34debcd3d1d5c37
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-12 23:34:53 +0300

    refactor HomePageTest

--- src/test/java/by/mybooks/ui/HomePageTest.java
+++ src/test/java/by/mybooks/ui/HomePageTest.java
@@ -2,7 +2,9 @@ package by.mybooks.ui;
 
 import by.mybooks.ui.homepage.HomePage;
 import by.mybooks.ui.homepage.HomePageMessage;
+import by.mybooks.ui.homepage.HomePageXpath;
 import by.mybooks.ui.steps.BasketStep;
+import by.mybooks.util.Books;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,10 +28,52 @@ public class HomePageTest extends BaseTest {
             "проверка, что книга добавилась в корзину")
     public void test2() {
 
-        BasketStep basketStep = new BasketStep();
-        basketStep.addBookIntoBasket();
+        BasketStep step = new BasketStep();
+        step.addBookIntoBasket(Books.HARRY_POTTER, HomePageXpath.BOOK_HARRY_POTTER_XPATH,
+                HomePageXpath.BUTTON_ADD_BOOK_HARRY_POTTER_INTO_BASKET_XPATH);
 
-        HomePage homePage  = new HomePage();
-        Assertions.assertEquals(HomePageMessage.THE_TITLE_OF_THE_BOOK_IN_THE_BASKET, homePage.getTextBookInTheBasket());
+        HomePage homePage = new HomePage();
+        Assertions.assertEquals(HomePageMessage.THE_TITLE_HARRY_POTTER,
+                homePage.getTextBookInTheBasket(HomePageXpath.BOOK_HARRY_POTTER_IN_THE_BASKET_XPATH));
+    }
+
+    @Test
+    @DisplayName("проверка открытия ссылки 'Оплата и доставка'")
+    public void test3() {
+
+        HomePage homePage = new HomePage();
+        homePage
+                .clickButtonAcceptAllCookies()
+                .clickPaymentAndDeliveryLink();
+
+        Assertions.assertEquals(HomePageMessage.PAYMENT_AND_DELIVERY_TEXT, homePage.getTextPaymentAndDelivery());
+    }
+
+    @Test
+    @DisplayName("поиск книги 'Голодные игры', добавление книги в корзину, " +
+            "проверка, что книга добавилась в корзину")
+    public void test4() {
+
+        BasketStep step = new BasketStep();
+        step.addBookIntoBasket(Books.THE_HUNGER_GAMES, HomePageXpath.BOOK_THE_HUNGER_GAMES_XPATH,
+                HomePageXpath.BUTTON_ADD_BOOK_THE_HUNGER_GAMES_INTO_BASKET_XPATH);
+
+        HomePage homePage = new HomePage();
+        Assertions.assertEquals(HomePageMessage.THE_TITLE_THE_HUNGER_GAMES,
+                homePage.getTextBookInTheBasket(HomePageXpath.BOOK_THE_HUNGER_GAMES_IN_THE_BASKET_XPATH));
+    }
+
+    @Test
+    @DisplayName("поиск книги 'Голодные игры', добавление книги в корзину, " +
+            "проверка, что книга добавилась в корзину")
+    public void test5() {
+
+        BasketStep step = new BasketStep();
+        step.addBookIntoBasket(Books.THE_HUNGER_GAMES, HomePageXpath.BOOK_THE_HUNGER_GAMES_XPATH,
+                HomePageXpath.BUTTON_ADD_BOOK_THE_HUNGER_GAMES_INTO_BASKET_XPATH);
+
+        HomePage homePage = new HomePage();
+        Assertions.assertEquals(HomePageMessage.THE_TITLE_THE_HUNGER_GAMES,
+                homePage.getTextBookInTheBasket(HomePageXpath.BOOK_THE_HUNGER_GAMES_IN_THE_BASKET_XPATH));
     }
 }

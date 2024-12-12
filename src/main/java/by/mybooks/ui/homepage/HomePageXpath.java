2e7f0592c9aa562bc845c092e2ee080e5ae8cfa6
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-12 23:33:39 +0300

    refactor HomePageXpath

--- src/main/java/by/mybooks/ui/homepage/HomePageXpath.java
+++ src/main/java/by/mybooks/ui/homepage/HomePageXpath.java
@@ -5,11 +5,13 @@ public class HomePageXpath {
     public static final String BUTTON_ACCEPT_ALL_COOKIES_XPATH = "//button[@data-button-ck-action='accept-all']";
     public static final String ADVANCED_SEARCH_LINK_XPATH = "//span[text()='Расширенный поиск']";
     public static final String PAYMENT_AND_DELIVERY_LINK_XPATH = "//span[text()='Оплата и доставка']";
+    public static final String PAYMENT_AND_DELIVERY_TEXT_XPATH = "//h1[text()='Оплата и доставка']";
     public static final String ADDRESS_OF_THE_SHOP_LINK_XPATH = "//span[text()='Адрес магазина']";
     public static final String ADDRESS_OF_THE_SHOO_TEXT_XPATH = "//div[@style=' padding: 20px; font-family: -webkit-body; ']" +
             "//span[contains(text(), 'Г. МИНСК, ОКТЯБРЬСКАЯ ПЛ. 2, ТЦ КУПАЛОВСКИЙ')]";
     public static final String SEARCH_STRING_XPATH = "//input[@placeholder='поиск']";
     public static final String BUTTON_SEARCH_XPATH = "//button[@type='submit']";
+    public static final String BUTTON_BASKET_XPATH = "//div[@class='ok-shcart-box']";
     public static final String IN_STOCK_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='В наличии']";
     public static final String NEW_YEAR_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Новый год']";
     public static final String NEW_PRODUCT_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Новинки']";
@@ -17,9 +19,15 @@ public class HomePageXpath {
     public static final String FICTION_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Художественная литература']";
     public static final String ESOTERICISM_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Эзотерика']";
     public static final String PSYCHOLOGY_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Психология']";
+
     public static final String BOOK_HARRY_POTTER_XPATH = "//div[@data-cart-id='798787']//div[@class='ok-product__main ']";
-    public static final String BUTTON_ADD_BOOK_INTO_BASKET_XPATH = "//meta[@data-id='1355176']//..//button" +
+    public static final String BUTTON_ADD_BOOK_HARRY_POTTER_INTO_BASKET_XPATH = "//meta[@data-id='1355176']//..//button" +
             "[@class='ok-product__add-shcart ok-btn -btn-shopping-cart ok-product__btn v-kozinu']";
-    public static final String BUTTON_BASKET_XPATH = "//div[@class='ok-shcart-box']";
-    public static final String BOOK_IN_THE_BASKET_XPATH = "//a[@href='/bkart/id-1034559431/']";
+    public static final String BOOK_HARRY_POTTER_IN_THE_BASKET_XPATH = "//a[@href='/bkart/id-1034559431/']";
+
+    public static final String BOOK_THE_HUNGER_GAMES_XPATH = "//div[@data-cart-id='312969']";
+    public static final String BUTTON_ADD_BOOK_THE_HUNGER_GAMES_INTO_BASKET_XPATH = "//meta[@data-id='1029007']//..//button" +
+            "[@data-gtm-id='add-to-cart-product']";
+    public static final String BOOK_THE_HUNGER_GAMES_IN_THE_BASKET_XPATH = "//a[@href=" +
+            "'/hudozhestvennaya_literatura/fantastika_fentezi_mistika/zarubezhnaya_fantastika/golodnye_igry_i_vspyhnet_plamya_soyka-peresmeshnica/']";
 }

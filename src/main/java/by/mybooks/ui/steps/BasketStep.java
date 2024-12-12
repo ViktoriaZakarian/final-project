9534cb1ccc2081f005792c450061f78e9d53ece1
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-13 00:15:22 +0300

    refactor BasketStep

--- src/main/java/by/mybooks/ui/steps/BasketStep.java
+++ src/main/java/by/mybooks/ui/steps/BasketStep.java
@@ -1,13 +1,16 @@
 package by.mybooks.ui.steps;
 
 import by.mybooks.ui.homepage.HomePage;
+import by.mybooks.ui.searchstring.SearchStringPage;
 
 public class BasketStep {
 
     public void addBookIntoBasket(String book, String bookXpath, String buttonAddTheBookIntoBasketXpath) {
         HomePage homePage = new HomePage();
-        homePage
-                .clickButtonAcceptAllCookies()
+        homePage.clickButtonAcceptAllCookies();
+
+        SearchStringPage searchStringPage = new SearchStringPage();
+        searchStringPage
                 .sendKeysSearchForABook(book)
                 .clickButtonSearch()
                 .clickButtonAddIntoBasket(bookXpath, buttonAddTheBookIntoBasketXpath)

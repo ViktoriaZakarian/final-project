3fc39775c4ffccca043cada26b81525f0047e3ff
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-13 00:13:47 +0300

    refactor HomePageMessage

--- src/main/java/by/mybooks/ui/homepage/HomePageMessage.java
+++ src/main/java/by/mybooks/ui/homepage/HomePageMessage.java
@@ -3,8 +3,5 @@ package by.mybooks.ui.homepage;
 public class HomePageMessage {
 
     public static final String ADDRESS_OF_THE_SHOP_TEXT = "Г. МИНСК, ОКТЯБРЬСКАЯ ПЛ. 2, ТЦ КУПАЛОВСКИЙ, ";
-    public static final String THE_TITLE_HARRY_POTTER = "Джоан Роулинг. Гарри Поттер и философский камень " +
-            "(оригинал Росмэн) | Роулинг Дж.К.";
-    public static final String THE_TITLE_THE_HUNGER_GAMES = "Голодные игры. И вспыхнет пламя. Сойка-пересмешница. Коллинз С.";
     public static final String PAYMENT_AND_DELIVERY_TEXT = "ОПЛАТА И ДОСТАВКА";
 }

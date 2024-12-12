78347d0236331faf60a9517a21db61b176ef380d
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-13 00:14:41 +0300

    add SearchStringMessage

---
+++ src/main/java/by/mybooks/ui/searchstring/SearchStringMessage.java
@@ -0,0 +1,9 @@
+package by.mybooks.ui.searchstring;
+
+public class SearchStringMessage {
+
+    public static final String THE_TITLE_HARRY_POTTER = "Джоан Роулинг. Гарри Поттер и философский камень " +
+            "(оригинал Росмэн) | Роулинг Дж.К.";
+    public static final String THE_TITLE_THE_HUNGER_GAMES = "Голодные игры. И вспыхнет пламя. Сойка-пересмешница. Коллинз С.";
+    public static final String THE_TITLE_THE_MAZE_RUNNER = "Бегущий в Лабиринте. Испытание огнем. Лекарство от смерти. Дэшнер Д.";
+}

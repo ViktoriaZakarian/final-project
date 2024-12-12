14340992452e6796c6eb2453ee51257b9918bda6
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-12 23:34:22 +0300

    add Books

---
+++ src/main/java/by/mybooks/util/Books.java
@@ -0,0 +1,8 @@
+package by.mybooks.util;
+
+public class Books {
+
+    public static final String HARRY_POTTER = "Гарри Поттер и философский камень";
+    public static final String THE_HUNGER_GAMES = "Голодные игры";
+    public static final String THE_MAZE_RUNNER = "Бегущий в лабиринте";
+}

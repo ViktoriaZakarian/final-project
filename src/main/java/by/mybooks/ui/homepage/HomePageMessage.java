da581da34aa6d67bd91df35cc541b3ca2232b31c
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-13 00:31:38 +0300

    refactor HomePageMessage

--- src/main/java/by/mybooks/ui/homepage/HomePageMessage.java
+++ src/main/java/by/mybooks/ui/homepage/HomePageMessage.java
@@ -4,4 +4,6 @@ public class HomePageMessage {
 
     public static final String ADDRESS_OF_THE_SHOP_TEXT = "Г. МИНСК, ОКТЯБРЬСКАЯ ПЛ. 2, ТЦ КУПАЛОВСКИЙ, ";
     public static final String PAYMENT_AND_DELIVERY_TEXT = "ОПЛАТА И ДОСТАВКА";
+    public static final String FICTION_TEXT = "ХУДОЖЕСТВЕННАЯ ЛИТЕРАТУРА";
+    public static final String NEW_YEAR_TEXT = "НОВОГОДНИЕ ПОДАРКИ";
 }

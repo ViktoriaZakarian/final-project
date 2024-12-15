b1f97950f2b3eb6f20ca92a6af95ea31f9a811c6
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-15 13:50:53 +0300

    refactor HomePageXpath

--- src/main/java/by/mybooks/ui/homepage/HomePageXpath.java
+++ src/main/java/by/mybooks/ui/homepage/HomePageXpath.java
@@ -4,22 +4,39 @@ public class HomePageXpath {
 
     public static final String BUTTON_ACCEPT_ALL_COOKIES_XPATH = "//button[@data-button-ck-action='accept-all']";
     public static final String ADVANCED_SEARCH_LINK_XPATH = "//span[text()='Расширенный поиск']";
+
     public static final String PAYMENT_AND_DELIVERY_LINK_XPATH = "//span[text()='Оплата и доставка']";
     public static final String PAYMENT_AND_DELIVERY_TEXT_XPATH = "//h1[text()='Оплата и доставка']";
+
     public static final String ADDRESS_OF_THE_SHOP_LINK_XPATH = "//span[text()='Адрес магазина']";
     public static final String ADDRESS_OF_THE_SHOP_TEXT_XPATH = "//div[@style=' padding: 20px; font-family: -webkit-body; ']" +
             "//span[contains(text(), 'Г. МИНСК, ОКТЯБРЬСКАЯ ПЛ. 2, ТЦ КУПАЛОВСКИЙ')]";
+
     public static final String SEARCH_STRING_XPATH = "//input[@placeholder='поиск']";
     public static final String BUTTON_SEARCH_XPATH = "//button[@type='submit']";
     public static final String BUTTON_BASKET_XPATH = "//div[@class='ok-shcart-box']";
+
     public static final String IN_STOCK_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='В наличии']";
+
     public static final String NEW_YEAR_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Новый год']";
     public static final String NEW_YEAR_TEXT_XPATH = "//h1[contains(text(), 'Новогодние подарки')]";
+
     public static final String NEW_PRODUCT_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Новинки']";
     public static final String BOOKS_FOR_CHILDREN_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Книги для детей']";
+
     public static final String FICTION_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Художественная литература']";
     public static final String FICTION_TEXT_XPATH = "//h1[contains(text(), 'Художественная литература')]";
+
     public static final String ESOTERICISM_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Эзотерика']";
     public static final String PSYCHOLOGY_LINK_XPATH = "//a[@class='ok-menu-top__href']//span[text()='Психология']";
     public static final String BUTTON_NEXT_XPATH = "//i[@class='fa fa-chevron-right']";
+
+    public static final String BESTSELLERS_LINK_XPATH = "//span[text()='Бестселлеры']";
+    public static final String BESTSELLERS_TEXT_XPATH = "//h1[@class='title']";
+
+    public static final String COMPUTER_LITERATURE_LINK_XPATH = "//span[text()='Компьютерная литература']";
+    public static final String COMPUTER_LITERATURE_TEXT_XPATH = "//h1[contains(text(), 'Компьютерная литература')]";
+
+    public static final String MEDICAL_LITERATURE_LINK_XPATH = "//span[text()='Медицинская литература']";
+    public static final String MEDICAL_LITERATURE_TEXT_XPATH = "//h1[contains(text(), 'Медицинская литература')]";
 }

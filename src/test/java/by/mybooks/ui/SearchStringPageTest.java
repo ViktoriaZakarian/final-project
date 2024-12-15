a1badad4d384c18987da775bd13a58e00c63be7f
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-15 13:52:39 +0300

    refactor SearchStringPageTest

--- src/test/java/by/mybooks/ui/SearchStringPageTest.java
+++ src/test/java/by/mybooks/ui/SearchStringPageTest.java
@@ -52,4 +52,22 @@ public class SearchStringPageTest extends BaseTest {
         Assertions.assertEquals(SearchStringMessage.THE_TITLE_THE_MAZE_RUNNER,
                 searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_THE_MAZE_RUNNER_IN_THE_BASKET_XPATH));
     }
+
+    @Test
+    @DisplayName("поиск книги 'Убийство в Восточном Экспрессе', добавление книги в корзину, увеличение количества = 2" +
+            "проверка, что 2 книги добавились в корзину")
+    public void test4() {
+
+        BasketStep step = new BasketStep();
+        step.addBookIntoBasket(Books.MURDER_ON_THE_ORIENT_EXPRESS, SearchStringXpath.BOOK_MURDER_ON_THE_ORIENT_EXPRESS_XPATH,
+                SearchStringXpath.BUTTON_ADD_BOOK_MURDER_ON_THE_ORIENT_EXPRESS_INTO_BASKET_XPATH);
+
+        SearchStringPage searchStringPage = new SearchStringPage();
+        searchStringPage.clickButtonAddTheSameExtraBook();
+
+        Assertions.assertEquals(SearchStringMessage.THE_TITLE_MURDER_ON_THE_ORIENT_EXPRESS,
+                searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_MURDER_ON_THE_ORIENT_EXPRESS_IN_THE_BASKET_XPATH));
+        Assertions.assertEquals(SearchStringMessage.THE_AMOUNT_OF_BOOKS_2,
+                searchStringPage.getAmountOfBooksInTheBasket(SearchStringXpath.THE_AMOUNT_OF_BOOKS_IN_THE_BASKET_XPATH));
+    }
 }

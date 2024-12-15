22fe84aaf462dde3d854713f69f7430e57648093
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-15 13:51:49 +0300

    refactor Books

--- src/main/java/by/mybooks/util/Books.java
+++ src/main/java/by/mybooks/util/Books.java
@@ -5,4 +5,5 @@ public class Books {
     public static final String HARRY_POTTER = "Гарри Поттер и философский камень";
     public static final String THE_HUNGER_GAMES = "Голодные игры";
     public static final String THE_MAZE_RUNNER = "Бегущий в лабиринте";
+    public static final String MURDER_ON_THE_ORIENT_EXPRESS = "Убийство в восточном экспрессе";
 }

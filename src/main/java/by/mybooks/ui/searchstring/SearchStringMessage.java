0393876751135b941094041437371a19622edf32
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-15 13:52:17 +0300

    refactor SearchStringMessage

--- src/main/java/by/mybooks/ui/searchstring/SearchStringMessage.java
+++ src/main/java/by/mybooks/ui/searchstring/SearchStringMessage.java
@@ -6,4 +6,6 @@ public class SearchStringMessage {
             "(оригинал Росмэн) | Роулинг Дж.К.";
     public static final String THE_TITLE_THE_HUNGER_GAMES = "Голодные игры. И вспыхнет пламя. Сойка-пересмешница. Коллинз С.";
     public static final String THE_TITLE_THE_MAZE_RUNNER = "Бегущий в Лабиринте. Испытание огнем. Лекарство от смерти. Дэшнер Д.";
+    public static final String THE_TITLE_MURDER_ON_THE_ORIENT_EXPRESS = "Убийство в \"Восточном экспрессе\". Кристи А.";
+    public static final String THE_AMOUNT_OF_BOOKS_2 = "2";
 }

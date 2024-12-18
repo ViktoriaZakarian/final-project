4161d554619749b8d705f9263731566d13e4afcd
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-19 01:39:42 +0300

    refactor SearchStringPageTest

--- src/test/java/by/mybooks/ui/SearchStringPageTest.java
+++ src/test/java/by/mybooks/ui/SearchStringPageTest.java
@@ -1,5 +1,6 @@
 package by.mybooks.ui;
 
+import by.mybooks.ui.homepage.HomePageXpath;
 import by.mybooks.ui.searchstring.SearchStringMessage;
 import by.mybooks.ui.searchstring.SearchStringPage;
 import by.mybooks.ui.searchstring.SearchStringXpath;
@@ -72,7 +73,7 @@ public class SearchStringPageTest extends BaseTest {
                 searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_THE_MAZE_RUNNER_IN_THE_BASKET_XPATH));
 
         logger.info("КОНЕЦ - testAddBookTheMazeRunner() поиск книги 'Бегущий в лабиринте', " +
-                "добавление книги в корзину, проверка, что книга добавилась в корзин");
+                "добавление книги в корзину, проверка, что книга добавилась в корзину");
     }
 
     @Test
@@ -97,5 +98,91 @@ public class SearchStringPageTest extends BaseTest {
 
         logger.info("КОНЕЦ - testAddBookMurderOnTheOrientExpress() поиск книги 'Убийство в Восточном Экспрессе', " +
                 "добавление книги в корзину, увеличение количества = 2, проверка, что 2 книги добавились в корзину");
+    }
+
+    @Test
+    @DisplayName("переход по ссылке 'Смысл' в разделе 'Психология', добавление книги 'Диагностика апатии' в корзину, " +
+            "проверка, что книга добавилась в корзину")
+    public void testAddBookDiagnosisOfApathy() {
+
+        logger.info("НАЧАЛО - testAddBookDiagnosisOfApathy() переход по ссылке 'Смысл' в разделе 'Психология', " +
+                "добавление книги 'Диагностика апатии' в корзину, проверка, что книга добавилась в корзину");
+
+        BasketStep step = new BasketStep();
+        step.addBookIntoBasketFromTheFirstPageOfCatalog(HomePageXpath.PSYCHOLOGY_LINK_XPATH, SearchStringXpath.SENSE_LINK_XPATH,
+                SearchStringXpath.BOOK_DIAGNOSIS_OF_APATHY_XPATH, SearchStringXpath.BUTTON_ADD_BOOK_DIAGNOSIS_OF_APATHY_INTO_BASKET_XPATH);
+
+        SearchStringPage searchStringPage = new SearchStringPage();
+
+        Assertions.assertEquals(SearchStringMessage.THE_TITLE_DIAGNOSIS_OF_APATHY,
+                searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_DIAGNOSIS_OF_APATHY_IN_THE_BASKET_XPATH));
+
+        logger.info("КОНЕЦ - testAddBookDiagnosisOfApathy() переход по ссылке 'Смысл' в разделе 'Психология', " +
+                "добавление книги 'Диагностика апатии' в корзину, проверка, что книга добавилась в корзину");
+    }
+
+    @Test
+    @DisplayName("переход по ссылке 'Традиция' в разделе 'История', добавление книги 'Круги на незримой воде' в корзину, " +
+            "проверка, что книга добавилась в корзину")
+    public void testAddBookCirclesOnInvisibleWater() {
+
+        logger.info("НАЧАЛО - testAddBookCirclesOnInvisibleWater() переход по ссылке 'Традиция' в разделе 'История', " +
+                "добавление книги 'Круги на незримой воде' в корзину, проверка, что книга добавилась в корзину");
+
+        BasketStep step = new BasketStep();
+        step.addBookIntoBasketFromTheSecondPageOfCatalog(HomePageXpath.HISTORY_LINK_XPATH, SearchStringXpath.TRADITION_LINK_XPATH,
+                SearchStringXpath.BOOK_CIRCLES_ON_INVISIBLE_WATER_XPATH, SearchStringXpath.BUTTON_ADD_BOOK_CIRCLES_ON_INVISIBLE_WATER_INTO_BASKET_XPATH);
+
+        SearchStringPage searchStringPage = new SearchStringPage();
+
+        Assertions.assertEquals(SearchStringMessage.THE_TITLE_CIRCLES_ON_INVISIBLE_WATER,
+                searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_CIRCLES_ON_INVISIBLE_WATER_IN_THE_BASKET_XPATH));
+
+        logger.info("КОНЕЦ - testAddBookCirclesOnInvisibleWater() переход по ссылке 'Традиция' в разделе 'История', " +
+                "добавление книги 'Круги на незримой воде' в корзину, проверка, что книга добавилась в корзину");
+    }
+
+    @Test
+    @DisplayName("переход по ссылке 'Книги для родителей' в разделе 'Детям и родителям', добавление книги " +
+            "'Французские дети не капризничают' в корзину, проверка, что книга добавилась в корзину")
+    public void testAddBookFrenchChildrenAreNotNaughty() {
+
+        logger.info("НАЧАЛО - testAddBookFrenchChildrenAreNotNaughty() переход по ссылке 'Книги для родителей' в разделе " +
+                "'Детям и родителям', добавление книги 'Французские дети не капризничают' в корзину, проверка, что книга добавилась в корзину");
+
+        BasketStep step = new BasketStep();
+        step.addBookIntoBasketFromTheThirdPageOfCatalog(HomePageXpath.FOR_CHILDREN_AND_PARENTS,
+                SearchStringXpath.BOOKS_FOR_PARENTS_LINK_XPATH, SearchStringXpath.BOOK_FRENCH_CHILDREN_ARE_NOT_NAUGHTY_XPATH,
+                SearchStringXpath.BUTTON_ADD_BOOK_FRENCH_CHILDREN_ARE_NOT_NAUGHTY_INTO_BASKET_XPATH);
+
+        SearchStringPage searchStringPage = new SearchStringPage();
+
+        Assertions.assertEquals(SearchStringMessage.THE_TITLE_FRENCH_CHILDREN_ARE_NOT_NAUGHTY,
+                searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_FRENCH_CHILDREN_ARE_NOT_NAUGHTY_IN_THE_BASKET_XPATH));
+
+        logger.info("КОНЕЦ - testAddBookFrenchChildrenAreNotNaughty() переход по ссылке 'Книги для родителей' в разделе " +
+                "'Детям и родителям', добавление книги 'Французские дети не капризничают' в корзину, проверка, что книга добавилась в корзину");
+    }
+
+    @Test
+    @DisplayName("переход по ссылке 'Лабиринт' в разделе 'Гуманитарные издания', добавление книги " +
+            "'Жестокий Принц' в корзину, проверка, что книга добавилась в корзину")
+    public void testAddBookCruelPrince() {
+
+        logger.info("НАЧАЛО - testAddBookCruelPrince() переход по ссылке 'Лабиринт' в разделе 'Гуманитарные издания', " +
+                "добавление книги 'Жестокий Принц' в корзину, проверка, что книга добавилась в корзину");
+
+        BasketStep step = new BasketStep();
+        step.addBookIntoBasketFromTheFourthPageOfCatalog(HomePageXpath.HUMANITIES_PUBLISHING_HOUSES_XPATH,
+                SearchStringXpath.MAZE_LINK_XPATH, SearchStringXpath.BOOK_CRUEL_PRINCE_XPATH,
+                SearchStringXpath.BUTTON_ADD_BOOK_CRUEL_PRINCE_INTO_BASKET_XPATH);
+
+        SearchStringPage searchStringPage = new SearchStringPage();
+
+        Assertions.assertEquals(SearchStringMessage.THE_TITLE_CRUEL_PRINCE,
+                searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_CRUEL_PRINCE_IN_THE_BASKET_XPATH));
+
+        logger.info("НАЧАЛО - testAddBookCruelPrince() переход по ссылке 'Лабиринт' в разделе 'Гуманитарные издания', " +
+                "добавление книги 'Жестокий Принц' в корзину, проверка, что книга добавилась в корзину");
     }
 }

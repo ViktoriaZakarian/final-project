9e0500ca33593b30ce5134df9c4a5b9914618a58
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-19 01:38:10 +0300

    refactor SearchStringXpath

--- src/main/java/by/mybooks/ui/searchstring/SearchStringXpath.java
+++ src/main/java/by/mybooks/ui/searchstring/SearchStringXpath.java
@@ -10,8 +10,8 @@ public class SearchStringXpath {
     public static final String BOOK_THE_HUNGER_GAMES_XPATH = "//div[@data-cart-id='312969']";
     public static final String BUTTON_ADD_BOOK_THE_HUNGER_GAMES_INTO_BASKET_XPATH = "//meta[@data-id='1029007']//..//button" +
             "[@data-gtm-id='add-to-cart-product']";
-    public static final String BOOK_THE_HUNGER_GAMES_IN_THE_BASKET_XPATH = "//a[@href=" +
-            "'/hudozhestvennaya_literatura/fantastika_fentezi_mistika/zarubezhnaya_fantastika/golodnye_igry_i_vspyhnet_plamya_soyka-peresmeshnica/']";
+    public static final String BOOK_THE_HUNGER_GAMES_IN_THE_BASKET_XPATH = "//a[@href='/hudozhestvennaya_literatura/" +
+            "fantastika_fentezi_mistika/zarubezhnaya_fantastika/golodnye_igry_i_vspyhnet_plamya_soyka-peresmeshnica/']";
 
     public static final String BOOK_THE_MAZE_RUNNER_XPATH = "//meta[@content='2']//..//div[@class='ok-product__main ']";
     public static final String BUTTON_ADD_BOOK_THE_MAZE_RUNNER_INTO_BASKET_XPATH = "//meta[@data-id='1029020']" +
@@ -20,9 +20,43 @@ public class SearchStringXpath {
             "'/hudozhestvennaya_literatura/fantastika_fentezi_mistika/zarubezhnaya_fantastika/Beguschiy_v_Labirinte2/']";
 
     public static final String BOOK_MURDER_ON_THE_ORIENT_EXPRESS_XPATH = "//div[@data-cart-id='836120']";
-    public static final String BUTTON_ADD_BOOK_MURDER_ON_THE_ORIENT_EXPRESS_INTO_BASKET_XPATH ="//meta[@data-id='1308108']" +
+    public static final String BUTTON_ADD_BOOK_MURDER_ON_THE_ORIENT_EXPRESS_INTO_BASKET_XPATH = "//meta[@data-id='1308108']" +
             "//..//button[@data-gtm-id='add-to-cart-product']";
-    public static final String BOOK_MURDER_ON_THE_ORIENT_EXPRESS_IN_THE_BASKET_XPATH = "//a[@href='/Novinki/ubiystvo-v-vostochnom-ekspresse-kristi-a/']";
+    public static final String BOOK_MURDER_ON_THE_ORIENT_EXPRESS_IN_THE_BASKET_XPATH = "//a[@href=" +
+            "'/Novinki/ubiystvo-v-vostochnom-ekspresse-kristi-a/']";
     public static final String BUTTON_ADD_THE_SAME_EXTRA_BOOK_XPATH = "//div[@class='ok-input-number__more']";
     public static final String THE_AMOUNT_OF_BOOKS_IN_THE_BASKET_XPATH = "//input[@id='item_836120']";
+
+    public static final String SENSE_LINK_XPATH = "//a[@class='ok-menu-top__href']//..//*//a[contains(text(),'Смысл')]";
+    public static final String BOOK_DIAGNOSIS_OF_APATHY_XPATH = "//a[@title='Диагностика апатии. Золотарева А.А.']" +
+            "//..//..//..//..//div[@class='ok-product__main ']";
+    public static final String BUTTON_ADD_BOOK_DIAGNOSIS_OF_APATHY_INTO_BASKET_XPATH = "//meta[@data-id='1119600']" +
+            "//..//button[@data-gtm-id='add-to-cart-product']";
+    public static final String BOOK_DIAGNOSIS_OF_APATHY_IN_THE_BASKET_XPATH = "//a[@href='/psihologiya-2/diagnostika-apatii/']";
+
+    public static final String TRADITION_LINK_XPATH = "//a[@class='ok-menu-top__href']//..//*//a[contains(text(),'Традиция')]";
+    public static final String BOOK_CIRCLES_ON_INVISIBLE_WATER_XPATH = "//meta[@content='Круги на незримой воде, Терр К., " +
+            "978-5-6044875-0-1, Традиция']//..//div[@class='ok-product__main ']";
+    public static final String BUTTON_ADD_BOOK_CIRCLES_ON_INVISIBLE_WATER_INTO_BASKET_XPATH = "//meta[@data-id='1349618']" +
+            "//..//button[@data-gtm-id='add-to-cart-product']";
+    public static final String BOOK_CIRCLES_ON_INVISIBLE_WATER_IN_THE_BASKET_XPATH = "//a[@href=" +
+            "'/istoriya-3/tradiciya/krugi-na-nezrimoy-vode-terr-k/']";
+
+    public static final String BOOKS_FOR_PARENTS_LINK_XPATH = "//a[@class='ok-menu-top__href']//..//*//a[contains(text()," +
+            "'Книги для родителей')]";
+    public static final String BOOK_FRENCH_CHILDREN_ARE_NOT_NAUGHTY_XPATH = "//meta[@content='Французские дети не капризничают. " +
+            "Уникальный опыт. Кроуфорд, Эксмо. 978-5-699-67558-6']//..//div[@class='ok-product__main ']";
+    public static final String BUTTON_ADD_BOOK_FRENCH_CHILDREN_ARE_NOT_NAUGHTY_INTO_BASKET_XPATH = "//meta[@data-id='1045574']" +
+            "//..//button[@data-gtm-id='add-to-cart-product']";
+    public static final String BOOK_FRENCH_CHILDREN_ARE_NOT_NAUGHTY_IN_THE_BASKET_XPATH = "//a[@href=" +
+            "'/detyam_i_roditelyam/knigi_dlya_roditeley/vospitanie_i_pedagogika/francuzskie_deti_ne_kapriznichayut" +
+            "_unikalnyy_opyt_parizhskogo_vospitaniya/']";
+
+    public static final String MAZE_LINK_XPATH = "//a[@class='ok-menu-top__href']//..//*//a[contains(text(),'Лабиринт')]";
+    public static final String BOOK_CRUEL_PRINCE_XPATH = "//meta[@content='Жестокий принц, Блэк Х., ЭКСМО, " +
+            "978-5-04-090470-9']//..//div[@class='ok-product__main ']";
+    public static final String BUTTON_ADD_BOOK_CRUEL_PRINCE_INTO_BASKET_XPATH = "//meta[@data-id='1119912']//.." +
+            "//button[@data-gtm-id='add-to-cart-product']";
+    public static final String BOOK_CRUEL_PRINCE_IN_THE_BASKET_XPATH = "//a[@href=" +
+            "'/novinki-gumanitarnyh-izdatelstv/NovLab/zhestokiy-princ/']";
 }

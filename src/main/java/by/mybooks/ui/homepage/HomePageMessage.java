5ab9bf6cdf2dfdb031fe0881b6c457be3431b0ad
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-21 04:36:46 +0300

    refactor HomePageXpath

--- src/main/java/by/mybooks/ui/homepage/HomePageMessage.java
+++ src/main/java/by/mybooks/ui/homepage/HomePageMessage.java
@@ -4,10 +4,27 @@ public class HomePageMessage {
 
     public static final String ADDRESS_OF_THE_SHOP_TEXT = "Г. МИНСК, ОКТЯБРЬСКАЯ ПЛ. 2, ТЦ КУПАЛОВСКИЙ, ";
     public static final String PAYMENT_AND_DELIVERY_TEXT = "ОПЛАТА И ДОСТАВКА";
-    public static final String FICTION_TEXT = "ХУДОЖЕСТВЕННАЯ ЛИТЕРАТУРА";
     public static final String NEW_YEAR_TEXT = "НОВОГОДНИЕ ПОДАРКИ";
+    public static final String IN_STOCK_TEXT = "В НАЛИЧИИ";
+    public static final String NEW_PRODUCTS_TEXT = "НОВИНКИ";
+    public static final String BOOKS_FOR_CHILDREN_TEXT = "КНИГИ ДЛЯ ДЕТЕЙ";
+    public static final String FICTION_TEXT = "ХУДОЖЕСТВЕННАЯ ЛИТЕРАТУРА";
+    public static final String ESOTERICISM_TEXT = "ЭЗОТЕРИКА";
+    public static final String PSYCHOLOGY_TEXT = "ПСИХОЛОГИЯ";
+    public static final String PHILOSOPHY_SOCIOLOGY_TEXT = "ФИЛОСОФИЯ. СОЦИОЛОГИЯ";
     public static final String BESTSELLERS_TEXT = "БЕСТСЕЛЛЕРЫ";
+    public static final String MARKDOWN_TEXT = "УЦЕНКА";
+    public static final String HISTORY_TEXT = "ИСТОРИЯ";
+    public static final String URSS_SCIENCE_EDUCATION_JURISPRUDENCE_TEXT = "УРСС НАУКА, ОБРАЗОВАНИЕ, ЮРИСПРУДЕНЦИЯ";
+    public static final String BUSINESS_LITERATURE_TEXT = "ДЕЛОВАЯ ЛИТЕРАТУРА";
+    public static final String FOR_CHILDREN_AND_PARENTS_TEXT = "ДЕТЯМ И РОДИТЕЛЯМ";
+    public static final String EDUCATIONAL_LITERATURE_TEXT = "УЧЕБНАЯ ЛИТЕРАТУРА";
+    public static final String ART_CULTURE_TEXT = "ИСКУССТВО. КУЛЬТУРА";
     public static final String COMPUTER_LITERATURE_TEXT = "КОМПЬЮТЕРНАЯ ЛИТЕРАТУРА";
     public static final String MEDICAL_LITERATURE_TEXT = "МЕДИЦИНСКАЯ ЛИТЕРАТУРА";
-    public static final String IN_STOCK_TEXT = "В НАЛИЧИИ";
+    public static final String NONFICTION_TEXT = "НЕХУДОЖЕСТВЕННАЯ ЛИТЕРАТУРА";
+    public static final String HUMANITIES_PUBLISHING_HOUSES_TEXT = "ГУМАНИТАРНЫЕ ИЗДАТЕЛЬСТВА";
+    public static final String OTHER_PRODUCTS_TEXT = "ПРОЧИЕ ТОВАРЫ";
+    public static final String HOME_CIRCLE_TEXT = "ДОМАШНИЙ КРУГ";
+    public static final String MANN_IVANOV_FERBER_TEXT = "МАНН, ИВАНОВ И ФЕРБЕР";
 }

6d5517134454dfe2ae4cae9d54f4f161876e7e1b
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-24 00:53:22 +0300

    refactor SearchStringPageTest

--- src/test/java/by/mybooks/ui/SearchStringPageTest.java
+++ src/test/java/by/mybooks/ui/SearchStringPageTest.java
@@ -9,12 +9,21 @@ import by.mybooks.util.Books;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SearchStringPageTest extends BaseTest {
 
     private static final Logger logger = LogManager.getLogger();
+    private BasketStep step;
+    private SearchStringPage searchStringPage;
+
+    @BeforeEach
+    void setUpSearchStringPage() {
+        searchStringPage = new SearchStringPage();
+        step = new BasketStep();
+    }
 
     @Test
     @DisplayName("поиск книги 'Гарри Поттер и философский камень', добавление книги в корзину, " +
@@ -24,11 +33,9 @@ public class SearchStringPageTest extends BaseTest {
         logger.info("НАЧАЛО - testAddBookHarryPotterIntoBasket() поиск книги 'Гарри Поттер и философский камень', " +
                 "добавление книги в корзину, проверка, что книга добавилась в корзину");
 
-        BasketStep step = new BasketStep();
         step.addBookIntoBasket(Books.HARRY_POTTER, SearchStringXpath.BOOK_HARRY_POTTER_XPATH,
                 SearchStringXpath.BUTTON_ADD_BOOK_HARRY_POTTER_INTO_BASKET_XPATH);
 
-        SearchStringPage searchStringPage = new SearchStringPage();
         Assertions.assertEquals(SearchStringMessage.THE_TITLE_HARRY_POTTER,
                 searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_HARRY_POTTER_IN_THE_BASKET_XPATH));
 
@@ -44,11 +51,9 @@ public class SearchStringPageTest extends BaseTest {
         logger.info("НАЧАЛО - testAddBookTheHungerGamesIntoBasket() поиск книги 'Голодные игры', " +
                 "добавление книги в корзину, проверка, что книга добавилась в корзину");
 
-        BasketStep step = new BasketStep();
         step.addBookIntoBasket(Books.THE_HUNGER_GAMES, SearchStringXpath.BOOK_THE_HUNGER_GAMES_XPATH,
                 SearchStringXpath.BUTTON_ADD_BOOK_THE_HUNGER_GAMES_INTO_BASKET_XPATH);
 
-        SearchStringPage searchStringPage = new SearchStringPage();
         Assertions.assertEquals(SearchStringMessage.THE_TITLE_THE_HUNGER_GAMES,
                 searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_THE_HUNGER_GAMES_IN_THE_BASKET_XPATH));
 
@@ -64,11 +69,9 @@ public class SearchStringPageTest extends BaseTest {
         logger.info("НАЧАЛО - testAddBookTheMazeRunner() поиск книги 'Бегущий в лабиринте', " +
                 "добавление книги в корзину, проверка, что книга добавилась в корзин");
 
-        BasketStep step = new BasketStep();
         step.addBookIntoBasket(Books.THE_MAZE_RUNNER, SearchStringXpath.BOOK_THE_MAZE_RUNNER_XPATH,
                 SearchStringXpath.BUTTON_ADD_BOOK_THE_MAZE_RUNNER_INTO_BASKET_XPATH);
 
-        SearchStringPage searchStringPage = new SearchStringPage();
         Assertions.assertEquals(SearchStringMessage.THE_TITLE_THE_MAZE_RUNNER,
                 searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_THE_MAZE_RUNNER_IN_THE_BASKET_XPATH));
 
@@ -84,11 +87,9 @@ public class SearchStringPageTest extends BaseTest {
         logger.info("НАЧАЛО - testAddBookMurderOnTheOrientExpress() поиск книги 'Убийство в Восточном Экспрессе', " +
                 "добавление книги в корзину, увеличение количества = 2, проверка, что 2 книги добавились в корзину");
 
-        BasketStep step = new BasketStep();
         step.addBookIntoBasket(Books.MURDER_ON_THE_ORIENT_EXPRESS, SearchStringXpath.BOOK_MURDER_ON_THE_ORIENT_EXPRESS_XPATH,
                 SearchStringXpath.BUTTON_ADD_BOOK_MURDER_ON_THE_ORIENT_EXPRESS_INTO_BASKET_XPATH);
 
-        SearchStringPage searchStringPage = new SearchStringPage();
         searchStringPage.clickButtonAddTheSameExtraBook();
 
         Assertions.assertEquals(SearchStringMessage.THE_TITLE_MURDER_ON_THE_ORIENT_EXPRESS,
@@ -108,11 +109,8 @@ public class SearchStringPageTest extends BaseTest {
         logger.info("НАЧАЛО - testAddBookDiagnosisOfApathy() переход по ссылке 'Смысл' в разделе 'Психология', " +
                 "добавление книги 'Диагностика апатии' в корзину, проверка, что книга добавилась в корзину");
 
-        BasketStep step = new BasketStep();
         step.addBookIntoBasketFromTheFirstPageOfCatalog(HomePageXpath.PSYCHOLOGY_LINK_XPATH, SearchStringXpath.SENSE_LINK_XPATH,
                 SearchStringXpath.BOOK_DIAGNOSIS_OF_APATHY_XPATH, SearchStringXpath.BUTTON_ADD_BOOK_DIAGNOSIS_OF_APATHY_INTO_BASKET_XPATH);
-
-        SearchStringPage searchStringPage = new SearchStringPage();
 
         Assertions.assertEquals(SearchStringMessage.THE_TITLE_DIAGNOSIS_OF_APATHY,
                 searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_DIAGNOSIS_OF_APATHY_IN_THE_BASKET_XPATH));
@@ -129,11 +127,8 @@ public class SearchStringPageTest extends BaseTest {
         logger.info("НАЧАЛО - testAddBookCirclesOnInvisibleWater() переход по ссылке 'Традиция' в разделе 'История', " +
                 "добавление книги 'Круги на незримой воде' в корзину, проверка, что книга добавилась в корзину");
 
-        BasketStep step = new BasketStep();
         step.addBookIntoBasketFromTheSecondPageOfCatalog(HomePageXpath.HISTORY_LINK_XPATH, SearchStringXpath.TRADITION_LINK_XPATH,
                 SearchStringXpath.BOOK_CIRCLES_ON_INVISIBLE_WATER_XPATH, SearchStringXpath.BUTTON_ADD_BOOK_CIRCLES_ON_INVISIBLE_WATER_INTO_BASKET_XPATH);
-
-        SearchStringPage searchStringPage = new SearchStringPage();
 
         Assertions.assertEquals(SearchStringMessage.THE_TITLE_CIRCLES_ON_INVISIBLE_WATER,
                 searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_CIRCLES_ON_INVISIBLE_WATER_IN_THE_BASKET_XPATH));
@@ -150,12 +145,9 @@ public class SearchStringPageTest extends BaseTest {
         logger.info("НАЧАЛО - testAddBookFrenchChildrenAreNotNaughty() переход по ссылке 'Книги для родителей' в разделе " +
                 "'Детям и родителям', добавление книги 'Французские дети не капризничают' в корзину, проверка, что книга добавилась в корзину");
 
-        BasketStep step = new BasketStep();
         step.addBookIntoBasketFromTheThirdPageOfCatalog(HomePageXpath.FOR_CHILDREN_AND_PARENTS_LINK_XPATH,
                 SearchStringXpath.BOOKS_FOR_PARENTS_LINK_XPATH, SearchStringXpath.BOOK_FRENCH_CHILDREN_ARE_NOT_NAUGHTY_XPATH,
                 SearchStringXpath.BUTTON_ADD_BOOK_FRENCH_CHILDREN_ARE_NOT_NAUGHTY_INTO_BASKET_XPATH);
-
-        SearchStringPage searchStringPage = new SearchStringPage();
 
         Assertions.assertEquals(SearchStringMessage.THE_TITLE_FRENCH_CHILDREN_ARE_NOT_NAUGHTY,
                 searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_FRENCH_CHILDREN_ARE_NOT_NAUGHTY_IN_THE_BASKET_XPATH));
@@ -172,12 +164,9 @@ public class SearchStringPageTest extends BaseTest {
         logger.info("НАЧАЛО - testAddBookCruelPrince() переход по ссылке 'Лабиринт' в разделе 'Гуманитарные издания', " +
                 "добавление книги 'Жестокий Принц' в корзину, проверка, что книга добавилась в корзину");
 
-        BasketStep step = new BasketStep();
         step.addBookIntoBasketFromTheFourthPageOfCatalog(HomePageXpath.HUMANITIES_PUBLISHING_HOUSES_LINK_XPATH,
                 SearchStringXpath.MAZE_LINK_XPATH, SearchStringXpath.BOOK_CRUEL_PRINCE_XPATH,
                 SearchStringXpath.BUTTON_ADD_BOOK_CRUEL_PRINCE_INTO_BASKET_XPATH);
-
-        SearchStringPage searchStringPage = new SearchStringPage();
 
         Assertions.assertEquals(SearchStringMessage.THE_TITLE_CRUEL_PRINCE,
                 searchStringPage.getTextBookInTheBasket(SearchStringXpath.BOOK_CRUEL_PRINCE_IN_THE_BASKET_XPATH));

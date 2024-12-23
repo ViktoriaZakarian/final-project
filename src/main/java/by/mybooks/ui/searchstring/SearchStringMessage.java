611d1e86288689a73e5d0a53de77ef8dd46cdebd
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-12-24 00:51:40 +0300

    refactor SearchStringMessage

--- src/main/java/by/mybooks/ui/searchstring/SearchStringMessage.java
+++ src/main/java/by/mybooks/ui/searchstring/SearchStringMessage.java
@@ -8,7 +8,7 @@ public class SearchStringMessage {
     public static final String THE_TITLE_THE_MAZE_RUNNER = "Бегущий в Лабиринте. Испытание огнем. Лекарство от смерти. Дэшнер Д.";
     public static final String THE_TITLE_MURDER_ON_THE_ORIENT_EXPRESS = "Убийство в \"Восточном экспрессе\". Кристи А.";
     public static final String THE_AMOUNT_OF_BOOKS_2 = "2";
-    public static final String THE_TITLE_DIAGNOSIS_OF_APATHY = "Диагностика апатии. Золотарева А.А.";
+    public static final String THE_TITLE_DIAGNOSIS_OF_APATHY = "Диагностика апатии.. Золотарева А.А.";
     public static final String THE_TITLE_CIRCLES_ON_INVISIBLE_WATER = "Круги на незримой воде. Терр К.";
     public static final String THE_TITLE_FRENCH_CHILDREN_ARE_NOT_NAUGHTY = "Французские дети не капризничают. " +
             "Уникальный опыт. Кроуфорд";
